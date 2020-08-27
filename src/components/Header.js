b98135e44c81a91cac7b79e2e99e48b748f4217b
Steven Ijames <ijamesproductions1@gmail.com>
2020-08-26 23:48:33 -0600

    heroku commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../styles/Header.css";
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>Employee Directory</h1>
+            <p>Type Employee Name In The Search Bar</p>
+        </div>
+    )
+}
+
+export default Header;
